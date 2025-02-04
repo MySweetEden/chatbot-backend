67858ae16908a2dd0deeb0ccd8234bdaf5610aff
mymissuniverse <mymissuniverse@icloud.com>
2025-02-04 22:54:06 +0900

    update: allow access

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return "Hello Root!"
 
 @app.get("/hello")
-def read_hello():
-    return {"hello": "world!"}
+async def read_hello():
+    return {"message": "Hello World"}
